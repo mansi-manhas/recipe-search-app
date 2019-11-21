f8e930f423b79a24c20596fa1d8b4c037511d4b2
Mansi <18692751+mansi-manhas@users.noreply.github.com>
2019-11-21 14:10:50 +0530

    Add files via upload

---
+++ recipe-app/src/components/Form.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Form = props => (
+    <form onSubmit={props.getRecipe} style={{ marginBottom:"2rem" }}>
+        <input className="form__input" type="text" name="recipeName"/>
+        <button className="button__input">Search</button>
+    </form>
+);
+
+export default Form;
---
+++ recipe-app/src/components/Recipe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Recipe extends React.Component{
+    render(){
+        console.log(this.props);
+        return(
+            <div>
+                Recipe Component
+            </div>
+        );
+    }
+};
+
+export default Recipe;
